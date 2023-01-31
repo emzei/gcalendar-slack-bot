06931233389303d5e0d8396a8f94b90a19578371
emzei <emzei.j@gmail.com>
2023-02-01 00:25:24 +0900

    Fix bugs
    - Fixed: bring events updated specific minutes ago
    - Fixed: All day events start,end
    - Fixed: Print event hours in a day
    - Fixed: Show correct date information for push notification
       (previous: Failed to load)

--- quickstart.py
+++ quickstart.py
@@ -19,6 +19,10 @@ SLACKBOT_RESTAPI = 'URL' #incoming-webhook url in your slack app
 GCALENDAR_ID = 'googlecalendar id to look for'
 SERVER_PORT = 30000
 
+PERIOD_MINUTES = 1
+SECONDS = 60
+UNIXTIME_DAY= 24*60*60
+
 def main():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
@@ -45,13 +49,15 @@ def main():
         service = build('calendar', 'v3', credentials=creds)
 
         # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print(now)
+        updated_ago = datetime.datetime.utcnow() - datetime.timedelta(minutes=PERIOD_MINUTES)
+        updated_isoformat = updated_ago.isoformat() + 'Z'  # 'Z' indicates UTC time
+
+        print(f'Updated schedules after', updated_isoformat)
         page_token = None
         calendar_id=GCALENDAR_ID
         toSlackBot=[]
         while True:
-            events = service.events().list(calendarId=calendar_id, pageToken=page_token, updatedMin=now).execute()
+            events = service.events().list(calendarId=calendar_id, pageToken=page_token, updatedMin=updated_isoformat).execute()
             for event in events['items']:
                 if event['status'] == 'confirmed':
                     if event['created'] == event['updated']:
@@ -68,22 +74,22 @@ def main():
             event = service.events().get(calendarId=calendar_id, eventId=eid).execute()
             #print(event['summary'],event['start'],event['end'] )
             s,e = '',''
-            date=[0,0]
+            date=[0,0] # pretty date
             allday = False
 
             if 'dateTime' in event['start']:
                 s,e = event['start']['dateTime'][:-6], event['end']['dateTime'][:-6]
-                tmp=time.strptime(s,'%Y-%m-%dT%H:%M:%S')
-                date[0] = str(time.mktime(tmp))[:-2]
-                tmp=time.strptime(e,'%Y-%m-%dT%H:%M:%S')
-                date[1] = str(time.mktime(tmp))[:-2]
+                tmp=datetime.datetime.strptime(s,'%Y-%m-%dT%H:%M:%S')
+                date[0] = str(time.mktime(tmp.timetuple()))[:-2]
+                tmp=datetime.datetime.strptime(e,'%Y-%m-%dT%H:%M:%S')
+                date[1] =  str(time.mktime(tmp.timetuple()))[:-2]
                 
             else:
                 s,e = event['start']['date'], event['end']['date']
-                tmp=time.strptime(s,'%Y-%m-%d')
-                date[0] = str(time.mktime(tmp))[:-2]
-                tmp=time.strptime(e,'%Y-%m-%d')
-                date[1] = str(time.mktime(tmp))[:-2]
+                tmp=datetime.datetime.strptime(s,'%Y-%m-%d')
+                date[0] = str(time.mktime(tmp.timetuple()))[:-2]
+                tmp=datetime.datetime.strptime(e,'%Y-%m-%d') - datetime.timedelta(days=1)
+                date[1] = str(time.mktime(tmp.timetuple()))[:-2]
                 allday=True
 
             title = ''
@@ -104,46 +110,57 @@ def main():
                 }
             })
             if allday:
-                if s == e:
+                if date[0]== date[1]:
+                    tmp_date = str(datetime.datetime.strptime(s,'%Y-%m-%d'))[:-9]
                     send_data['blocks'].append( 
                     { 
                         "type":"section", 
                         "text":
                             {
                                 "type":"mrkdwn",
-                                "text":f'<!date^{date[0]}'+'^ {date_long_pretty} |Fail to load time>(All day)'
+                                "text":f'<!date^{date[0]}'+'^ {date_long_pretty} | '+f'{tmp_date} (All day)>'
                             }
                         
                         }
                     )
                 else:
+                    tmp_date_s= str(datetime.datetime.strptime(s,'%Y-%m-%d'))[:-9]
+                    tmp_date_e= str(datetime.datetime.strptime(e,'%Y-%m-%d')-datetime.timedelta(days=1))[:-9]
                     send_data['blocks'].append( 
                     { 
                         "type":"section", 
                         "text":
                             {
                                 "type":"mrkdwn",
-                                "text":f'from <!date^{date[0]}'+'^ {date_long_pretty} |Fail to load time> to'+ \
-                                    f'<!date^{date[1]}'+'^ {date_long_pretty} | Fail to load time> '
+                                "text":f'from <!date^{date[0]}'+'^ {date_long_pretty} |'+f'{tmp_date_s}> to'+ \
+                                    f'<!date^{date[1]}'+'^ {date_long_pretty} | '+f'{tmp_date_e}> '
                             }
                         
                         }
                     )
             else:
+                tmp_date_s=str(datetime.datetime.strptime(s,'%Y-%m-%dT%H:%M:%S'))[:-3]
+                tmp_date_e=''
+                if int(date[1]) - int(date[0]) < UNIXTIME_DAY:
+                    tmp_date_e=datetime.datetime.strptime(e,'%Y-%m-%dT%H:%M:%S')
+                    tmp_date_e=str(tmp_date_e)[10:-3]
+
+                else:
+                    tmp_date_e=str(datetime.datetime.strptime(e,'%Y-%m-%dT%H:%M:%S'))[:-3]
+                
                 send_data['blocks'].append( 
                 { 
                     "type":"section", 
                     "text":
                         {
                             "type":"mrkdwn",
-                            "text":f'from <!date^{date[0]}'+'^ {date_long_pretty} {time}|Fail to load time> to'+ \
-                                f'<!date^{date[1]}'+'^ {date_long_pretty} {time}|Fail to load time> '
+                            "text":f'from <!date^{date[0]}'+'^ {date_long_pretty} {time}|'+f'{tmp_date_s}> to'+ \
+                                f'<!date^{date[1]}'+'^ {date_long_pretty} {time}|'+f'{tmp_date_e}> '
                         }
                     
                     }
                 )
             response=requests.post(SLACKBOT_RESTAPI, json=send_data)
-            #print(response.content)
             
             if response.status_code == 400:
                 print(response.content)
